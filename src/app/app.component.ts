c7fe4fce688fbcbb51090ec101399e3ee372bae0
Thanh Huỳnh <huynhnguyentuanthanh@gmail.com>
2023-02-17 19:01:19 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{result}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+    result = "Hello World";
+
+    constructor(private httpClient: HttpClient) {
+        this.httpClient.get<any>("https://lab03-api.azurewebsites.net/weatherforecast").subscribe((res)=>{
+            this.result = JSON.stringify(res);
+        });
+    }
 }
